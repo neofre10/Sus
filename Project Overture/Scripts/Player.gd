7226ba600a7e0d30eb66001d1c3a6f2c3814a7be
neofre10 <neofre10@kingshigh.school.nz>
2021-05-04 13:15:21 +1200

    Cycle1: Day 1
    Added test room and began script to player control

---
+++ Project Overture/Scripts/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody
+
+export var speed = 1
+export var acceleration = 0.5
+export var max_speed = 10
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var mouse_accel = -.3
+
+#func _input(event):
+	#if event is InputEventMouseMotion:
+		#rotate_y(deg2rad(event.get_relative().x * mouse_accel ))
+
+func get_input():
+	var input = Vector3(
+		-int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right")),
+		0,
+		-int(Input.is_action_pressed("ui_up")) + int(Input.is_action_pressed("ui_down"))
+	)
+	input = input.normalized()
+	return input
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var input = get_input()
+	if input != Vector3.ZERO:
+		speed += acceleration
+		if speed > max_speed:
+			speed = max_speed
+
+	var fwd = Vector3(global_transform.basis.z) * input.z * speed 
+	var strafe = Vector3(global_transform.basis.x) * input.x * speed 	
+
+	move_and_slide(fwd)
+	move_and_slide(strafe)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
